ac9911199ba788b7d9b2e6705cd84e6f2f96752c
Ujjwal <ujjwalborasi19jul@gmail.com>
2021-10-17 19:49:28 +0530

    UI Changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Matches from './Components/Matches/Matches';
 import Blogs from './Components/Blogs/Blogs'
 import Stadiums from './Components/Stadiums/Stadiums'
 import Navbar from './Components/NavBar/Navbar';
+import Home from './Components/Home/Home';
+
 
 function App() {
   return (
@@ -11,7 +13,8 @@ function App() {
       <BrowserRouter>
         <Navbar></Navbar>
         <Switch>
-          <Route exact path="/" component={Blogs} ></Route>
+          <Route exact path="/" component={Home} ></Route>
+          <Route exact path="/Blogs" component={Blogs} ></Route>
           <Route exact path="/Matches" component={Matches}></Route>
           <Route exact path="/Stadiums" component={Stadiums}></Route>
         </Switch>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-
+import {  NavLink  } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight  } from '@fortawesome/free-solid-svg-icons';
+import './Home.css'
 function Home() {
     return (
-       <>
-       
-       </>
+        <>
+            <div className='Main-Container'>
+                <div className='Middle-Container'>
+                    <div className='Button-Container'>
+                    <NavLink className="Home-Button" exact to="/Matches">
+                        <span className="Button-Text">CHECK LIST</span> <FontAwesomeIcon icon={faArrowRight} size="1px" className="Button-Icon" />
+                    </NavLink>
+                    <NavLink className="Home-Button" exact to="/Stadiums">
+                        <span className="Button-Text">SEE GALLERY</span> <FontAwesomeIcon icon={faArrowRight} size="1px" className="Button-Icon" />
+                    </NavLink>
+                    </div>
+                </div>
+            </div>
+
+        </>
     )
 }
 
